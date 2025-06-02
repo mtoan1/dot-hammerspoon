03fae9b44a756da28829797aab416c0df92c75c0
Your Name <you@example.com>
2025-06-02 21:43:53 +0700

    update

--- system_manage.lua
+++ system_manage.lua
@@ -18,35 +18,4 @@ hs.hotkey.bind(
     end
 )
 
--- Start screensaver.
-hs.hotkey.bind(
-    system.screen_saver.prefix,
-    system.screen_saver.key,
-    system.screen_saver.message,
-    function()
-        log.d("start screensaver")
-        hs.caffeinate.startScreensaver()
-    end
-)
-
--- Restart.
-hs.hotkey.bind(
-    system.restart.prefix,
-    system.restart.key,
-    system.restart.message,
-    function()
-        hs.caffeinate.restartSystem()
-    end
-)
-
--- Shutdown.
-hs.hotkey.bind(
-    system.shutdown.prefix,
-    system.shutdown.key,
-    system.shutdown.message,
-    function()
-        hs.caffeinate.shutdownSystem()
-    end
-)
-
 return _M
